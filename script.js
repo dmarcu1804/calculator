8432f24bbb020da5c4d7c23347280db7891ea69c
Dragos <dragos.ionut99@yahoo.com>
2023-03-28 13:36:28 +0100

    added functionality for basic operations(add, subtract, divide, multiply and equals)

--- script.js
+++ script.js
@@ -24,11 +24,53 @@ function operate(operator, num1, num2){
     else if(operator === '*'){
         return multiply(num1, num2);
     }
-    else{
+    else if(operator === '/'){
         return divide(num1,num2);
     }
 }
 
-const operator = '+';
+const displayCurrent = Array.from(document.getElementsByClassName("current-operand"))[0];
+const output = document.querySelector(".output");
+const displayPrevious = Array.from(document.getElementsByClassName("previous-operand"))[0];
+const buttons = Array.from(document.querySelectorAll('button'));
+const operatorSelector = document.querySelectorAll(".operator");
+const result = document.createElement('div');
+result.classList.add('result');
+output.appendChild(result);
+// console.log(output);
+const operators = [];
+const numbers = [];
+let temp = 0;
+let resFinal = 0;
+//seems to work now for most operations - need to adjust what happens for when equals is pressed
+buttons.forEach(button => button.addEventListener('click', (e) =>{
+    if(isFinite(e.target.value)){
+        displayCurrent.innerHTML += e.target.value;
+        result.innerText = "";
+        
+    }else{
+        temp = displayCurrent.textContent;
+        numbers.push(temp);
+        operators.push(e.target.value);
+        displayCurrent.innerHTML = "";
+        if(operators.length > 1){
+            if(operators[0] != "="){
+                resFinal = operate(operators[0], Number(numbers[0]), Number(numbers[1]));
+                numbers.splice(0,2);
+                numbers.push(resFinal);
+                operators.splice(0,1);
+                result.innerText = resFinal;
+            }else{
+                if(numbers[1] == ""){
+                    numbers.splice(1,1);
+                }
+                operators.splice(0,1);
+                resFinal = operate(operators[0], Number(numbers[0]), Number(numbers[1]));
+            }
+        }
+        displayCurrent.innerHTML = "";
+    }
+    
 
-console.log(operate(operator, 5,3));
+    
+}));
